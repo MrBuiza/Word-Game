e4173a255d381935d6ae7c3a35ddebc9abfcf88c
mateo <mateobuiza@hotmail.com>
2023-09-29 20:02:54 +0100

    A very big update.

--- src/Warrior.java
+++ src/Warrior.java
@@ -1,8 +1,7 @@
 public class Warrior extends Entity {
 
-
-    public Warrior(String name, int health, int attack){
-        super(name, health, attack);
+    public Warrior(String type, String name, int health, int attack){
+        super(type, name, health, attack);
     }
 
 }
